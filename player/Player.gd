d952dce74d0157365f021806bddd292aef703de2
Hugues Baratgin <hugues.baratgin@gmail.com>
2024-06-04 12:25:14 +0200

    feat: use device tilt to move player

--- player/Player.gd
+++ player/Player.gd
@@ -3,14 +3,27 @@ extends CharacterBody2D
 var jumpImpulse := 7.0 * 60
 var gravityImpulse := 8.0 * 60
 var movementSpeed := 3.0 * 60
-var dir: =0.0
 
+var tiltThreshold = 0.5
 
-func _physics_process(delta:float)->void:
-	dir=Input.get_action_strength("move_right")-Input.get_action_strength("move_left")
-	velocity.y+=gravityImpulse*delta
-	if is_on_floor():
-		velocity.y= -jumpImpulse
+func _physics_process(delta: float) -> void:
+	var tiltValue = Input.get_accelerometer().x
+
+	var horizontalMovement = 0
+	if  abs(tiltValue) > tiltThreshold:
+		horizontalMovement = tiltValue
+	velocity.x = horizontalMovement * movementSpeed
+
 	
-	velocity.x = dir * movementSpeed
+	if  Input.is_action_pressed("move_left"):
+		horizontalMovement = -3
+	if  Input.is_action_pressed("move_right"):
+		horizontalMovement = 3
+	velocity.x = horizontalMovement * movementSpeed
+		
+	velocity.y += gravityImpulse * delta
+	if is_on_floor():
+		velocity.y = -jumpImpulse
+	
 	move_and_slide()
+	
